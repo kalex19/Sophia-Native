6b37e1159098f6d6101f308645434eae436734e8
Andreea Hanson <andreea.hanson7@gmail.com>
2019-09-01 16:56:46 -0600

    Create AddListForm component

---
+++ AddListForm/AddListForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+class AddListForm extends Component {
+  state = {}
+
+  render() {
+    return(
+      <View style={styles.list}>
+        <View>
+          <Text>Here is the Form</Text>
+        </View>
+        <View>
+          <Text>Add List Title</Text>
+          <TextInput style={styles.input}></TextInput>
+          <Button title="ADD TITLE"/>
+        </View>
+        <View>
+          <Text>Add Item</Text>
+          <TextInput style={styles.input}></TextInput>
+          <Button title="ADD ITEM"/>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default AddListForm;
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 30,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginVertical: 10,
+    backgroundColor: 'white'
+  }
+});
--- App/App.js
+++ App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ClientProfile from '../ClientProfile/ClientProfile';
 import ClientList from '../ClientList/ClientList';
+import AddListForm from '../AddListForm/AddListForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ClientProfile />
       <ClientList />
+      <AddListForm />
     </View>
   );
 }
