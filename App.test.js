fa4fd4f7f11c53293f0b184750a41ce50a168cae
Katherine Lewis <kale8432@colorado.edu>
2019-09-04 09:46:44 -0600

    added App snpashot test

--- App.test.js
+++ App.test.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import App from './App';
-import renderer from 'react-test-renderer';
+// import React from 'react';
+// import App from './App';
+// import renderer from 'react-test-renderer';
 
-test('renders correctly', () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
+// test('renders correctly', () => {
+//   const tree = renderer.create(<App />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
+import React from 'react';
+import { App } from './App';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+	let wrapper, instance;
+
+	beforeEach(() => {
+		wrapper = shallow(
+			<App />
+		);
+		instance = wrapper.instance();
+	});
+
+	it(`matches snapshot`, () => {
+		expect(wrapper).toMatchSnapshot();
+  });
 });
