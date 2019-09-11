9e9a37939bb6751490085783674d6d892401347e
Andreea Hanson <andreea.hanson7@gmail.com>
2019-09-11 13:15:59 -0600

    Add handleChange test and first part of handleClientSubmit test

--- Components/CreateAccount/CreateAccount.test.js
+++ Components/CreateAccount/CreateAccount.test.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { CreateAccount } from './CreateAccount';
-import { shallow } from 'enzyme';
-import 'react-native';
+import React from "react";
+import { CreateAccount } from "./CreateAccount";
+import { shallow } from "enzyme";
+import "react-native";
 import { mapStateToProps, mapDispatchToProps } from "./CreateAccount";
-import { logIn } from '../../actions'
+import { logIn } from "../../actions";
 
 jest.mock("react-native-gesture-handler", () => {
-  return {}
+  return {};
 });
+
+jest.mock("../../Utils/apiCalls", () => ({
+  postClient: jest.fn().mockImplementation(() => {
+    return {
+      id: 1,
+      name: "mockUser"
+    };
+  })
+}));
 
 global.fetch = jest.fn().mockImplementation(() => {
   return Promise.resolve({
@@ -16,14 +25,21 @@ global.fetch = jest.fn().mockImplementation(() => {
   });
 });
 
-let wrapper = shallow(<CreateAccount />)
+let mockPostClient = jest.fn()
+let mockLogIn = jest.fn()
+
+let wrapper = shallow(<CreateAccount 
+postClient = {mockPostClient}
+logIn={mockLogIn}
+navigation={{ navigate: jest.fn() }}
+/>);
 
 test("CreateAccount renders correctly", () => {
   expect(wrapper).toMatchSnapshot();
 });
 
 it("should return a userAccount object", () => {
-  const initialState = {}
+  const initialState = {};
 
   let mappedProps = mapStateToProps(initialState);
 
@@ -43,3 +59,40 @@ it("calls dispatch with an object when logIn action is called", () => {
 
   expect(mockDispatch).toHaveBeenCalledWith(mockAction);
 });
+
+test("change the state of every element of the initial state when handleChange is invoked", () => {
+  wrapper.state.name = "";
+  let mockValue = "changed";
+
+  let mockName = "name";
+
+  wrapper.instance().handleChange(mockName, mockValue);
+
+  expect(wrapper.state("name")).toEqual(mockValue);
+});
+
+it("should set the message state to a string that asks the user to type in all the input fields when handleClientSubmit is invoked", () => {
+  wrapper.state.message = "";
+  wrapper.state.username = "filled"
+  wrapper.state.password = "filled"
+  wrapper.state.password_confirmation = "filled"
+  wrapper.state.name = ""
+  wrapper.state.email = "filled"
+  wrapper.state.phone = "filled"
+  wrapper.state.address = "filled"
+  wrapper.state.state = "filled"
+  wrapper.state.city = "filled"
+  wrapper.state.zip = "filled"
+  wrapper.state.needs = "filled"
+  wrapper.state.allergies = "filled"
+  wrapper.state.diet = "filled"
+  wrapper.state.medications = "filled"
+  let mockMessage = "Please fill out all input fields";
+
+  wrapper.instance().handleClientSubmit();
+
+  expect(wrapper.state("message")).toEqual(mockMessage);
+});
+
+
+
