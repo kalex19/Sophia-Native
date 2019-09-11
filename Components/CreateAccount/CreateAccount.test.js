c87ee7a5d8c2132e75ebfa86dc0c5ee1da95f480
Andreea Hanson <andreea.hanson7@gmail.com>
2019-09-11 12:24:41 -0600

    Add snapshot and mapStateToProps tests

--- Components/CreateAccount/CreateAccount.test.js
+++ Components/CreateAccount/CreateAccount.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import {CreateAccount} from './CreateAccount';
-import renderer from 'react-test-renderer';
+import { CreateAccount } from './CreateAccount';
+import { shallow } from 'enzyme';
 import 'react-native';
+import { mapStateToProps, mapDispatchToProps } from "./CreateAccount";
 
-test('CreateAccount renders correctly', () => {
-  const snapshot = renderer.create(<CreateAccount />).toJSON();
-  expect(snapshot).toMatchSnapshot();
+jest.mock("react-native-gesture-handler", () => {
+  return {}
 });
+
+global.fetch = jest.fn().mockImplementation(() => {
+  return Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve(mockUser)
+  });
+});
+
+let wrapper = shallow(<CreateAccount />)
+
+test("CreateAccount renders correctly", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("should return a userAccount object", () => {
+  const initialState = {}
+
+  let mappedProps = mapStateToProps(initialState);
+
+  expect(mappedProps).toEqual(initialState);
+});
+
+// it("calls dispatch with an array of lists when loadList action is called", () => {
+//   let initialState = {
+//     lists: []
+//   };
+
+//   const mockDispatch = jest.fn();
+//   const mockAction = loadLists(initialState.lists);
+
+//   const mappedProps = mapDispatchToProps(mockDispatch);
+//   mappedProps.loadLists(initialState.lists);
+
+//   expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+// });
