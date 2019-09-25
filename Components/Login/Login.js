619674d05a0c0f2403c210c5f52e8437e2adbdf1
Katie Lewis <39716292+kalex19@users.noreply.github.com>
2019-09-25 15:24:59 -0600

    Revert "Refactor Home and Login screens and extract Button and Input to standalone components"

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, KeyboardAvoidingView } from 'react-native';
+import { View, Text, TextInput, KeyboardAvoidingView } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 import { logIn } from '../../actions';
 import { logInUser } from '../../Utils/logInUser';
 import { PropTypes } from 'prop-types';
-import Button from '../common/Button/Button';
-import Input from '../common/Input/Input';
-import styles from './styles';
+import theme from '../../theme';
+import { styles } from './styleLogin';
 
 const initialState = {
 	username: '',
@@ -50,42 +50,50 @@ export class Login extends Component {
 		}
 	};
 
-	render() {
+	render () {
 		return (
 			<KeyboardAvoidingView style={styles.container} behavior="padding" enabled accessibile={true}>
 				<View style={styles.headerContainer}>
-					<Text style={styles.header}>Log In</Text>
+					<Text style={styles.header}> Log In </Text>
 				</View>
-				<Input
+				<TextInput
 					style={styles.input}
 					value={this.state.username}
 					onChangeText={value => this.handleChange('username', value)}
-					accessibilityLabel="Username Input"
 					placeholder="Username"
+					accessibilityLabel={'Username Input'}
+					placeholderTextColor={theme.primary}
 				/>
-				<Input
+				<TextInput
+					style={styles.input}
 					value={this.state.password}
+					placeholder="Password"
 					onChangeText={value => this.handleChange('password', value)}
-					accessibilityLabel="Password Input"
+					accessibilityLabel={'Password Input'}
 					minLength={8}
 					secureTextEntry={true}
-					placeholder="Password"
+					placeholderTextColor={theme.primary}
 				/>
-				<Button accessibilityLabel="Tap me to log into your account." onPress={this.handleSubmit}>
-					Log In
-				</Button>
-				<View style={{ position: 'relative', width: '100%', alignSelf: 'center' }}>
-					<Text style={styles.text} accessibilityLabel="Please type a username and password">
-						{this.state.message}
-					</Text>
-					<Text style={styles.text} accessibilityLabel="Incorrect username or password">
-						{this.state.error}
-					</Text>
+				<Text style={styles.text} accessibilityLabel={'Please type a username and password'}>
+					{this.state.message}
+				</Text>
+				<View style={styles.routes}>
+					<TouchableHighlight
+						underlayColor="black"
+						accessibilityLabel="Tap me to log into your account."
+						onPress={this.handleSubmit}
+						style={styles.touchExpander}>
+						<Text style={styles.button}> Log In </Text>
+					</TouchableHighlight>
 				</View>
+				<Text style={styles.text} accessibilityLabel={'Incorrect username or password'}>
+					{this.state.error}
+				</Text>
 			</KeyboardAvoidingView>
 		);
 	}
 }
+
 
 const mapStateToProps = state => ({
 	user: state.userAccount
@@ -95,10 +103,7 @@ export const mapDispatchToProps = dispatch => ({
 	logIn: user => dispatch(logIn(user))
 });
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
 
 Login.propTypes = {
 	userAccount: PropTypes.object
