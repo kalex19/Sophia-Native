c015a8bb8a48f5a8e275ad4023fca3fa7113374b
Katherine Lewis <kale8432@colorado.edu>
2019-09-25 17:20:10 -0600

    added action to addlist

--- actions/index.js
+++ actions/index.js
@@ -1,31 +1,36 @@
 export const loadProfile = profile => ({
-  type: "LOAD_PROFILE",
-  profile
-})
+	type: 'LOAD_PROFILE',
+	profile
+});
 
 export const loadLists = lists => ({
-  type: "LOAD_LISTS",
-  lists
-})
+	type: 'LOAD_LISTS',
+	lists
+});
 
-export const logIn = (user) => ({
-  type:'LOG_IN',
-  user
-})
+export const logIn = user => ({
+	type: 'LOG_IN',
+	user
+});
 
 export const loadTasks = tasks => ({
-  type: "LOAD_TASKS",
-  tasks
-})
+	type: 'LOAD_TASKS',
+	tasks
+});
 
 export const logOut = () => ({
-  type:'LOG_OUT'
-})
+	type: 'LOG_OUT'
+});
 
-export const togglecomplete = (bool) => ({
-  type: "TOGGLE_COMPLETE",
-  bool
-})
+export const togglecomplete = bool => ({
+	type: 'TOGGLE_COMPLETE',
+	bool
+});
+
+export const addList = list => ({
+	type: 'ADD_LIST',
+	list
+});
 
 // export const isLoading = (bool) => ({
 //       type: 'IS_LOADING',
