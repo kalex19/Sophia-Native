8b17182996951fd862a81c2ea4f566aef5df6e0d
Katherine Lewis <kale8432@colorado.edu>
2019-09-25 17:07:56 -0600

    Restyled profile

--- Components/Profile/styles.js
+++ Components/Profile/styles.js
@@ -1,60 +1,65 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 import theme from '../../theme';
-
- export const styles = StyleSheet.create({
-  profileContainer: {
-    margin: 30,
-  },
-  headerContainer: {
-    borderBottomColor:theme.primary,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    paddingBottom: 10
-  },
-  header: {
-    textAlign: "center",
-    fontSize: 30,
-    fontFamily: theme.textMain,
-    margin: 10,
-  },
-  userInfo: {
-    fontSize: 20,
-    fontFamily: theme.textTwo,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor:theme.primary,
-    color: theme.accentOne,
-    padding: 20
-  },
-  infoCntainer: {
-    backgroundColor: theme.primary,
-    marginTop: 10,
-    marginBottom: 10,
-    padding: 10
-  },
-  userInfoList: {
-    fontSize: 20,
-    fontFamily: theme.textTwo,
-    color: theme.accentOne,
-  },
-  touchExpander: {
-    height: "100%",
-    borderRadius: 50,
-    width: "100%"
-  },
-  logOutButton: {
-    color: theme.accentOne,
-    fontSize: 30,
-    fontFamily: theme.textTwo,
-    textAlign: "center",
-    paddingTop: 15,
-  },
-  routes: {
-		flexDirection: "column",
-    backgroundColor: theme.primary,
-    width: "90%",
-    height: "10%",
-    justifyContent: "center",
-    margin: 15,
-    borderRadius: 50,
+export default StyleSheet.create({
+	profileContainer: {
+		// margin: 30,
+		width: '100%'
+	},
+	headerContainer: {
+		borderBottomColor: theme.primary,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		marginBottom: 10
+	},
+	header: {
+		textAlign: 'center',
+		fontSize: 30,
+		fontFamily: theme.textMain,
+		margin: 10
+	},
+	userInfo: {
+		fontSize: 20,
+		fontFamily: theme.textTwo,
+		marginTop: 10,
+		marginBottom: 10,
+		// backgroundColor: theme.primary,
+		color: theme.accentTwo,
+		// padding: 20,
+		textAlign: 'left',
+		alignSelf: 'center',
+		width: '85%',
+		borderBottomColor: theme.accentTwo,
+		borderBottomWidth: 1
+	},
+	infoCntainer: {
+		backgroundColor: theme.primary,
+		marginTop: 10,
+		marginBottom: 10,
+		padding: 10
+	},
+	userInfoList: {
+		fontSize: 20,
+		fontFamily: theme.textTwo,
+		color: theme.accentOne
+	},
+	touchExpander: {
+		height: '100%',
+		borderRadius: 50,
+		width: '100%'
+	},
+	logOutButton: {
+		color: theme.accentOne,
+		fontSize: 30,
+		fontFamily: theme.textTwo,
+		textAlign: 'center',
+		paddingTop: 15
+	},
+	routes: {
+		flexDirection: 'column',
+		backgroundColor: theme.primary,
+		width: '90%',
+		height: '10%',
+		justifyContent: 'center',
+		margin: 15,
+		borderRadius: 50
 	}
 });
