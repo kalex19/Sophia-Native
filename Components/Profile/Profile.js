0f287bc97b68bd016755ba20952a14bc51d6752d
Katherine Lewis <kale8432@colorado.edu>
2019-09-27 14:02:27 -0600

    Refactor profile to display image and CR edit btn

--- Components/Profile/Profile.js
+++ Components/Profile/Profile.js
@@ -1,49 +1,93 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Input } from 'react-native';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import styles from './styles';
 import Button from '../common/Button/Button';
 import Header from '../common/Header/Header';
+import Input from '../common/Input/Input';
+
+const initialState = {
+	username: '',
+	password: '',
+	street_address: '',
+	city: '',
+	state: '',
+	zip: '',
+	allergies: '',
+	needs: '',
+	diet_restrictions: '',
+	medications: '',
+	abilities: '',
+	displayEdit: ''
+};
 
 export class Profile extends Component {
+	state = initialState;
+
+	// returnUpdatedProfile = async () => {
+	// 	const lists = await fetchClientLists(this.props.user.id);
+	// 	this.props.loadLists(lists);
+	// };
+
+	toggleEdit = list_id => {
+		this.setState({ displayEdit: list_id });
+	};
+
+	handleEdit = value => {
+		this.setState({ [name]: value });
+	};
+
+	handleSubmitEdit = async listId => {
+		const { user } = this.props;
+		if (this.props.user.role === 'client') {
+			const updatedProfile = {
+				username: this.state.username,
+				password: this.state.password,
+				street_address: this.state.street_address,
+				city: this.state.city,
+				state: this.state.state,
+				zip: this.state.zip,
+				allergies: this.state.allergies,
+				needs: this.state.needs,
+				diet_restrictions: this.state.diet_restrictions,
+				medications: this.state.medications,
+				list_id: listId,
+				client_id: user.id
+			};
+			await patchClientList(updatedProfile);
+		} else {
+			const updatedProfile = {
+				username: this.state.username,
+				password: this.state.password,
+				abilities: this.state.abilities
+			};
+			await patchCaretakerList(updatedProfile);
+		}
+		this.returnUpdatedProfile();
+		this.setState({ initialState });
+	};
 
 	renderClientInfo = () => {
 		let allNeeds = this.props.user.needs.map(need => {
-			return (
-				<Text style={styles.userInfoList} key={Math.random()}>
-					- {need}
-				</Text>
-			);
+			return <Input style={styles.userInfoList} key={Math.random()} name="needs" value={need} />;
 		});
 		let allMedications = this.props.user.medications.map(med => {
-			return (
-				<Text style={styles.userInfoList} key={Math.random()}>
-					- {med}
-				</Text>
-			);
+			return <Input style={styles.userInfoList} key={Math.random()} name="medications" value={med} />;
 		});
 		let allAllergies = this.props.user.allergies.map(allergy => {
-			return (
-				<Text style={styles.userInfoList} key={Math.random()}>
-					- {allergy}
-				</Text>
-			);
+			return <Input style={styles.userInfoList} key={Math.random()} name="allergies" value={allergy} />;
 		});
 		let allRestrictions = this.props.user.diet_restrictions.map(restr => {
-			return (
-				<Text style={styles.userInfoList} key={Math.random()}>
-					- {restr}
-				</Text>
-			);
+			return <Input style={styles.userInfoList} key={Math.random()} name="diet_restrictions" value={restr} />;
 		});
 		return (
 			<View>
 				<View style={styles.addressContainer}>
-				<Text style={styles.userInfo}>Street Address: {this.props.user.street_address}</Text>
-				<Text style={styles.userInfo}>{this.props.user.city}</Text>
-				<Text style={styles.userInfo}> {this.props.user.state}</Text>
-				<Text style={styles.userInfo}> {this.props.user.zip}</Text>
+					<Input style={styles.userInfo} name="street_address" value={this.props.user.address} />
+					<Input style={styles.userInfo} name="city" value={this.props.user.city} />
+					<Input style={styles.userInfo} name="state" value={this.props.user.state} />
+					<Input style={styles.userInfo} name="zip" value={this.props.user.zip} />
 				</View>
 				<View style={styles.infoContainer}>
 					<Text style={styles.userInfo}>Needs:</Text>
@@ -67,9 +111,7 @@ export class Profile extends Component {
 	renderCaretakerInfo = () => {
 		let allAbilities = this.props.user.abilities.map(ablility => {
 			return (
-				<Text style={styles.userInfoList} key={Math.random()}>
-					- {ablility}
-				</Text>
+				<Input style={styles.userInfoList} key={Math.random()} name="abilities" value={this.props.user.abilties} />
 			);
 		});
 		return (
@@ -84,11 +126,24 @@ export class Profile extends Component {
 			<View>
 				<Header>My Profile</Header>
 				<image src="../assets/stockFace.jpg" style={styles.image}></image>
-				<Text style={styles.userInfo}> {this.props.user.name}</Text>
+				<Input style={styles.userInfo}> {this.props.user.name}</Input>
+				{this.state.displayEdit !== list.id && (
+					<Button accessibilityLabel="Tap me to edit your profile." onPress={() => this.toggleEditName(list.id)}>
+						Edit Profile
+					</Button>
+				)};
+				{this.state.displayEdit === list.id && (
+					<Button
+						accessibilityLabel="Tap me to submit your edited profile."
+						onPress={() => this.handleSubmitEdit(list.id)}
+					>
+						Save Profile
+					</Button>
+				)};
 				<ScrollView style={styles.container}>
-					<Text style={styles.userInfo}>Username: {this.props.user.username}</Text>
-					<Text style={styles.userInfo}>Email: {this.props.user.email}</Text>
-					<Text style={styles.userInfo}>Phone Number: {this.props.user.phone_number}</Text>
+					<Input style={styles.userInfo}>Username: {this.props.user.username}</Input>
+					<Input style={styles.userInfo}>Email: {this.props.user.email}</Input>
+					<Input style={styles.userInfo}>Phone Number: {this.props.user.phone_number}</Input>
 					{this.props.user.role === 'client' && this.renderClientInfo()}
 					{this.props.user.role === 'caretaker' && this.renderCaretakerInfo()}
 					<View style={{ height: 150 }}></View>
@@ -100,9 +155,7 @@ export class Profile extends Component {
 const mapStateToProps = state => ({
 	user: state.userAccount
 });
-const mapDispatchToProps = dispatch => ({
-
-});
+const mapDispatchToProps = dispatch => ({});
 export default connect(
 	mapStateToProps,
 	mapDispatchToProps
