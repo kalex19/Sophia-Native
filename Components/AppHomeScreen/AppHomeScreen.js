ef84598b0b38911ef483b91b2da466299aaf1992
Andreea Hanson <andreea.hanson7@gmail.com>
2019-09-30 15:16:30 -0600

    Correct error message when the username/email already exists
    Co-Authored-By: Katherine Lewis <kale8432@colorado.edu>

--- Components/AppHomeScreen/AppHomeScreen.js
+++ Components/AppHomeScreen/AppHomeScreen.js
@@ -3,10 +3,11 @@ import { View, Text } from 'react-native';
 import { styles } from './styles';
 import Header from '../common/Header/Header';
 import Button from '../common/Button/Button';
+import theme from '../../theme';
 
 const AppHomeScreen = props => {
 	return (
-		<View style={styles.container}>
+		<View style={theme.container}>
 			<Header style={{ fontSize: 60 }} accessibilityLabel="Speech Operated Personal Household Interactive Assistant">
 				SOPHIA
 			</Header>
--- Components/AppHomeScreen/styles.js
+++ Components/AppHomeScreen/styles.js
@@ -1,10 +1,4 @@
 import { StyleSheet } from 'react-native';
 import theme from '../../theme';
 
-export const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
-}
-});
+export const styles = StyleSheet.create({});
