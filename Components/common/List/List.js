b618c8dccdc7343c07283873486a1d629a58289e
Katherine Lewis <kale8432@colorado.edu>
2019-10-01 07:36:15 -0600

    Style list in need done

--- Components/common/List/List.js
+++ Components/common/List/List.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { View, TouchableHighlight, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import styles from './styles';
+import Button from '../Button/Button';
 
 export const List = ({ list, navigation, user }) => {
 	return (
-		<View style={styles.lists} key={list.id} accessible={true}>
-			<TouchableHighlight
-				underlayColor="black"
+		<View style={styles.cotainer} key={list.id}>
+			{list.role === 'client' && <Text style={styles.name}>{list.caretaker_name}'s list</Text>}
+			{list.role === 'caretaker' && <Text style={styles.name}>{list.client_name}'s list</Text>}
+			<Button
 				accessibilityLabel={`Tap me to navigate to your ${list.name} list. From there view your tasks.`}
-				accessible={true}
-				style={styles.listName}
 				onPress={() => {
 					navigation.navigate('NeedToDoTasks', list);
 				}}
-			>
-				<Text style={styles.name}>{list.name}</Text>
-			</TouchableHighlight>
-			{list.role === 'client' && <Text style={styles.name}>{list.caretaker_name}'s list</Text>}
-			{list.role === 'caretaker' && <Text style={styles.name}>{list.client_name}'s list</Text>}
-		</View>
+				>
+				{list.name}
+			</Button>
+				</View>
 	);
 };
--- Components/common/List/styles.js
+++ Components/common/List/styles.js
@@ -2,26 +2,17 @@ import { StyleSheet } from 'react-native';
 import theme from '../../../theme';
 
 export default StyleSheet.create({
-	listItem: {
-		fontSize: 25,
-		color: theme.accentOne,
-		padding: 5
-	},
-	lists: {
+	container: {
+		width: '100%',
 		flexDirection: 'column',
 		justifyContent: 'space-between',
-		backgroundColor: theme.primary,
 		alignItems: 'center',
 		margin: 10,
 		padding: 10
 	},
-	listName: {
-		color: theme.accentOne,
-		fontSize: 40,
-		fontFamily: theme.textTwo
-	},
 	name: {
-		color: "white",
-		fontSize: 25
+		color: theme.accentTwo,
+		fontSize: 25,
+		fontFamily: theme.textMain
 	}
 });
