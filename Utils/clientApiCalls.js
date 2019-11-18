3843c628b235d5de4aa63af35e6680529abe766c
Andreea Hanson <andreea.hanson7@gmail.com>
2019-11-18 16:27:28 -0700

    Refactor tests for clientApiCalls

--- Utils/clientApiCalls.js
+++ Utils/clientApiCalls.js
@@ -56,7 +56,6 @@ export const postClientTask = async (newTask, list_id, client_id) => {
 };
 
 export const deleteClientTask = async (list_id, task_id) => {
-	console.log("LIST ID", list_id, "TASK ID", task_id)
 	const options = {
 		method: 'DELETE',
 		headers: { 'Content-Type': 'application/json' }
@@ -91,15 +90,15 @@ export const fetchClients = async () => {
 };
 
 export const patchClientProfile = async (updatedProfile, user_id) => {
-	console.log('updated profile', updatedProfile);
-	console.log('user id', user_id);
+	// console.log('updated profile', updatedProfile);
+	// console.log('user id', user_id);
 	const options = {
 		method: 'PATCH',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(updatedProfile)
 	};
 	const response = await fetch(`https://evening-dusk-50121.herokuapp.com/api/v1/clients/${user_id}`, options);
-	console.log('response', response)
+	// console.log('response', response)
 
 	if (!response.ok) {
 		throw new Error('Could not edit the client profile');
